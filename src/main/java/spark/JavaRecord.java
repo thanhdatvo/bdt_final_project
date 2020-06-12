daac5d3455c90630adc83afcb14e547c86007c48
datvt9312 <datvt9312@gmail.com>
2020-06-11 21:54:59 -0500

    first commit

---
+++ src/main/java/spark/JavaRecord.java
@@ -0,0 +1,13 @@
+package spark;
+
+public class JavaRecord implements java.io.Serializable {
+	private String word;
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+}
