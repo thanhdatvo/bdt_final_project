7335eeeac6643987d85e713962cfbd3862ab63c8
datvt9312 <datvt9312@gmail.com>
2020-06-13 16:06:01 -0500

    done push data from spark sql to kibana

---
+++ src/main/java/spark/OnlineStore.java
@@ -0,0 +1,106 @@
+package spark;
+
+import java.io.Serializable;
+
+public class OnlineStore implements Serializable {
+	private static final long serialVersionUID = 2369441390379166257L;
+	private String eventTime;
+	private String eventType;	
+	private String productId;
+	private String categoryId;
+	private String categoryCode;
+	private String brand;
+	private String price;
+	private String userId;
+	private String userSession;
+
+	public OnlineStore(String eventTime
+			,
+			String eventType, String productId, String categoryId, String categoryCode,
+			String brand, String price, String userId, String userSession
+			) {
+		super();
+		this.setEventTime(eventTime);
+		this.setEventType(eventType);
+		this.productId = productId;
+		this.categoryId = categoryId;
+		this.categoryCode = categoryCode;
+		this.brand = brand;
+		this.price = price;
+		this.userId = userId;
+		this.userSession = userSession;
+	}
+	public String getEventTime() {
+		return eventTime;
+	}
+
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime;
+	}
+
+	public String getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserSession() {
+		return userSession;
+	}
+
+	public void setUserSession(String userSession) {
+		this.userSession = userSession;
+	}
+	
+
+}
---
+++ src/main/java/spark/SparkElasticsearch.java
@@ -0,0 +1,59 @@
+package spark;
+
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Counts words in UTF8 encoded, '\n' delimited text received from the network.
+ *
+ * Usage: JavaStructuredNetworkOnlineStoreCount <hostname> <port> <hostname> and
+ * <port> describe the TCP server that Structured Streaming would connect to
+ * receive data.
+ *
+ * To run this on your local machine, you need to first run a Netcat server `$
+ * nc -lk 9999` and then run the example `$ bin/run-example
+ * sql.streaming.JavaStructuredNetworkOnlineStoreCount localhost 9999`
+ */
+public final class SparkElasticsearch {
+	
+	public static void main(String[] args) throws Exception {
+		if (args.length < 2) {
+			System.err.println("Usage: JavaStructuredNetworkOnlineStoreCount <hostname> <port>");
+			System.exit(1);
+		}
+
+		String host = args[0];
+		int port = Integer.parseInt(args[1]);
+
+		SparkSession spark = SparkSession.builder().appName("JavaStructuredNetworkOnlineStoreCount").master("local[*]")
+				.config("es.index.auto.create", "true").config("es.nodes.wan.only", "true")
+				.config("es.nodes.wan.only", "true")
+				.config("es.nodes", "localhost").config("es.port", "9200").getOrCreate();
+
+		spark.sparkContext().setLogLevel("ERROR");
+
+
+		Dataset<Row> lines = spark.readStream().format("socket").option("host", host).option("port", port).load();
+
+		Encoder<OnlineStore> onlineStoreEncoder = Encoders.bean(OnlineStore.class);
+
+		Dataset<OnlineStore> onlineStoreData = lines.as(Encoders.STRING()).map((MapFunction<String, OnlineStore>) x -> {
+			String[] properties = x.split(",");
+			return new OnlineStore(properties[0]
+					, 
+					properties[1], properties[2], properties[3], properties[4],
+					properties[5], properties[6], properties[7], properties[8]
+							);
+		}, onlineStoreEncoder);
+		
+		onlineStoreData.writeStream()
+		.format("org.elasticsearch.spark.sql")
+				.option("checkpointLocation", "/home/checkpointLocation7").start("online_stores/_doc")
+				.awaitTermination();
+
+	}
+}
